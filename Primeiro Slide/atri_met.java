2144fb759956feb05b2e8e4aa6e755ba813caa59
guinho <guilhermeribeiroifba@gmail.com>
2024-10-01 15:01:48 -0300

    Add files via upload

---
+++ Primeiro Slide/atri_met.java
@@ -0,0 +1,14 @@
+/*
+ class Carro {
+    // Atributos
+    String marca;
+    String modelo;
+    int ano;
+
+    // Métodos
+    void acelerar() {
+        // Exibe na tela "O carro acelerou."
+        System.out.println("O carro acelerou.");
+    }
+}
+ */
---
+++ Primeiro Slide/objetos.java
@@ -0,0 +1,10 @@
+/*
+ class Main {
+    public static void main(String args[]) {
+        Carro c1 = new Carro();
+        c1.marca = "Fiat";
+        c1.modelo = "Uno";
+        c1.acelerar();
+    }
+}
+ */
